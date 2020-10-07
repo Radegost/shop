40e6d6c82bb433fc90ae1565b04ff70e13e17b73
kchojnecki <konrad.chojnecki@gmail.comgit config --global user.email konrad.chojnecki@gmail.comgit config --global user.email konrad.chojnecki@gmail.com>
2020-10-07 21:03:25 +0200

    add spring boot app module

---
+++ application/src/main/java/com/kc/app/Shop.java
@@ -0,0 +1,12 @@
+package com.kc.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shop {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Shop.class, args);
+    }
+}
